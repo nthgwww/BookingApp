0859d0547c992d7658ecf7fc27227aea4a3d782d
Ngoc Thong Ho <ngocthonggl01@gmail.com>
2025-04-12 15:04:33 +0700

    update

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,21 @@
 import express from 'express';
 import { deleteUser, getAllUser, getUser, updateUser } from '../controllers/user.js';
+import { verifyAdmin, verifyToken, verifyUser } from '../utils/verifyToken.js';
 
 const router = express.Router();
+
+router.get("/checkauthentication", verifyToken, (req,res,next)=>{
+    res.send("hello user, you are logged in")
+})
+
+router.get("/checkuser/:id", verifyUser, (req,res,next)=>{
+    res.send("hello user, you are logged in and you can edit your account")
+})
+
+router.get("/checkadmin/:id", verifyAdmin, (req,res,next)=>{
+    res.send("hello admin, you are logged in and you can edit all accounts")
+})
+
 
 //UPDATE
 router.put("/:id", updateUser)
